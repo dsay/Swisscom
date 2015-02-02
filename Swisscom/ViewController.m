583b9ee27871141ea35ddbc50e5ce9fa4bdca78b
Dima <dsay@ciklum.com>
2015-02-02 11:14:53 +0200

    clear code

--- Swisscom/ViewController.m
+++ Swisscom/ViewController.m
@@ -1,11 +1,3 @@
-//
-//  ViewController.m
-//  Swisscom
-//
-//  Created by Dima on 1/30/15.
-//  Copyright (c) 2015 Dima Sai. All rights reserved.
-//
-
 #import "ViewController.h"
 #import "SynchronizationService.h"
 #import "DataStorage.h"
@@ -16,27 +8,22 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    
+
     [[SynchronizationService shared] startSynchronizationSuccess:^(BOOL success) {
         [self fetchData];
     }];
+    
     [self fetchData];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)fetchData
 {
     DataStorage *dataStorage = [DataStorage shared];
     NSFetchRequest *request = [dataStorage.requestBuilder questions];
-    NSArray *questions = [dataStorage performFetchRequest:request];
-
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSArray *question = [dataStorage performFetchRequest:request];
 }
 
 @end
